1b327d5bbaa2f5b44e5dcfd18f329cf3d5d8ebfb
tonyenra <tonyenra@gmail.com>
2025-07-23 20:51:11 -0400

    Django Proxy Services of Backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('logout', views.logout_user, name='logout'),
     path('register', views.register_user, name='register'),
     path('get_cars', views.get_cars, name='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
